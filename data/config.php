b4e68e5e98afec2d97efc0c271746e182b533ce4
Daniel Berry <dpberry@usca.edu>
2017-12-30 16:02:19 -0500

    Update config.php

--- data/config.php
+++ data/config.php
@@ -6,14 +6,14 @@ define('EMAIL_CONTACT_RECIPIENT','laura@sharpeconceptsny.com');
 
 $mysql_config = [
 	'MYSQL_USER' => 'www',
-	'MYSQL_PASSWORD' => '75142a39$F',
+	'MYSQL_PASSWORD' => '*******',
 	'MYSQL_CONNECTION_STRING' => 'mysql:host=localhost;dbname=sharpe'
 ];
 $email_config = [
 	'SMTP_SERVER' => 'localhost',
 	'EMAIL_USER' => 'mailer',
 	'EMAIL_USERNAME' => 'mailer@sharpeconceptsproject',
-	'EMAIL_PASSWORD' => '1o9f5erTa@f',
+	'EMAIL_PASSWORD' => '*******',
 	'EMAIL_CONTACT_RECIPIENT' => 'dpberry@usca.edu',
 	'EMAIL_FROM' => 'donotreply@sharpeconceptsproject',
 	'EMAIL_PORT' => 587
